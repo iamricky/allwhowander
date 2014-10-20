1e121e8b0e4f86148bb4140c24384d06b0c46bfc
heynicecode <rickymuldrew@gmail.com>
2014-10-20 15:06:47 -0400

    Create home page

---
+++ index.php
@@ -0,0 +1,22 @@
+
+		<?php get_header(); ?>
+
+			<section class="jumbotron">
+				<div id="Fader" class="fader">
+					<img class="slide" src="<?php bloginfo("template_url"); ?>/img/slider/slider-img-01.jpg"/>
+					<img class="slide" src="<?php bloginfo("template_url"); ?>/img/slider/slider-img-02.jpg"/>
+					<img class="slide" src="<?php bloginfo("template_url"); ?>/img/slider/slider-img-03.jpg"/>
+					<img class="slide" src="<?php bloginfo("template_url"); ?>/img/slider/slider-img-04.jpg"/>
+					<div class="fader_controls">
+						<ul class="pager_list"></ul>
+					</div>
+				</div>
+			</section>
+			
+			<div class="row">
+				<div class="col-sm-10">
+					<h6 class="edgy lg">authentic and refined events, with an edge</h6>
+				</div>
+			</div>
+			
+			<?php get_footer(); ?>
