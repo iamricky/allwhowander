34cf77f0c9bcae935eca82db9a5ec49e17e11645
heynicecode <rickymuldrew@gmail.com>
2014-10-21 21:23:38 -0400

    Create 'get_template_part' for social meda icons

--- footer.php
+++ footer.php
@@ -1,20 +1,6 @@
-					
-			<div class="row hidden-xs">
-				<div class="col-sm-7">
-					<img src="<?php bloginfo("template_url"); ?>/img/social-stroke.png" class="img-responsive social-stroke-align" >
-				</div>
-				
-				<div class="col-sm-5">
-					<span class="edgy footer">follow along</span>
-					<ul class="socialnav">
-						<li class="facebook"><a href="https://www.facebook.com/allwhowanderevents" target="_blank"></a></li>
-						<li class="twitter"><a href="https://twitter.com/wanderevents" target="_blank"></a></li>
-						<li class="pinterest"><a href="http://www.pinterest.com/whowander/" target="_blank"></a></li>
-						<li class="instagram"><a href="http://instagram.com/allwhowanderevents" target="_blank"></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>  <!-- container end -->
+
+			<?php get_template_part( "content", "social" ); ?>
+		</div> <!-- container end -->
 		<footer>
 			<div class="custom-border"></div>
 			<div class="container">
@@ -45,7 +31,7 @@
 		<?php wp_footer(); ?>
 		<script>
 		$(document).ready(function(){
-			$(".feature-init").click(function(){ 
+			$(".feature-init").click(function(){
 				$(".my-panel").fadeToggle();
 			});
 			$('.portfolio-pg [title], [alt]').each( function(){
