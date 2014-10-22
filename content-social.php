67349f89955396b06dd9a9ba265b3cb81a4a654d
heynicecode <rickymuldrew@gmail.com>
2014-10-21 21:23:38 -0400

    Create 'get_template_part' for social meda icons

---
+++ content-social.php
@@ -0,0 +1,18 @@
+<div class="row hidden-xs">
+    <div class="col-sm-7">
+        <img src="<?php bloginfo("template_url"); ?>/img/social-stroke.png" class="img-responsive social-stroke-align" >
+    </div>
+
+    <div class="col-sm-5">
+        <span class="edgy footer">follow along</span>
+        <ul class="socialnav">
+            <li class="facebook"><a href="https://www.facebook.com/allwhowanderevents" target="_blank"></a></li>
+
+            <li class="twitter"><a href="https://twitter.com/wanderevents" target="_blank"></a></li>
+
+            <li class="pinterest"><a href="http://www.pinterest.com/whowander/" target="_blank"></a></li>
+
+            <li class="instagram"><a href="http://instagram.com/allwhowanderevents" target="_blank"></a></li>
+        </ul>
+    </div>
+</div>
