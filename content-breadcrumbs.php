b15f9ffc9ae629456dd964c23bed5f222722ea12
heynicecode <rickymuldrew@gmail.com>
2014-10-23 20:46:38 -0400

    Create breadcrumbs for 'Services' page

---
+++ content-breadcrumbs.php
@@ -0,0 +1,16 @@
+<div class="row services-pg-breadcrumb">
+    <div class="col-sm-3">
+        <h6>• <a title="one" rel="services-nav">planning &amp; full design</a>
+        </h6>
+    </div>
+    <div class="col-sm-3">
+        <h6>• <a title="two" rel="services-nav">planning &amp; partial design</a></h6>
+    </div>
+    <div class="col-sm-3">
+        <h6>• <a title="three" rel="services-nav">month-of planning</a></h6>
+    </div>
+    <div class="col-sm-3">
+        <h6>•<a title="four" rel="services-nav">looking for something else ?</a>
+        </h6>
+    </div>
+</div>
