359c08c1e7a0c296404231e5ae1401b7d6b00129
heynicecode <rickymuldrew@gmail.com>
2014-10-23 20:45:55 -0400

    Create 'Page' template

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+<?php
+
+$page_title	= strtolower( get_the_title() );
+$body_class	= array( "row", "no-margin", $page_title . "-pg" );
+
+?>
+
+	<?php if ( !is_page( "about" ) ) : ?>
+		<section class="jumbotron hero">
+			<img src="<?php bloginfo( "template_url" ); ?>/img/hero/<?php echo $page_title ?>.jpg" class="img-responsive">
+		</section>
+	<?php endif; ?>
+
+	<div class="row headline">
+		<h1><?php the_title(); ?></h1>
+		<img src="<?php bloginfo( "template_url" ); ?>/img/social-stroke.png" class="img-responsive social-stroke-align hidden-xs">
+	</div>
+
+	<div id="post-<?php the_ID(); ?>" <?php post_class( $body_class ); ?>>
+		<?php if ( is_page( "service" ) ) : ?>
+		<?php get_template_part( "content", "breadcrumbs" ); endif; ?>
+		<?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
+	</div>
+
+<?php get_footer(); ?>
