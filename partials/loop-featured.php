6f08ebacde16b53d1c3a1b430d9c2af569474c4a
heynicecode <rickymuldrew@gmail.com>
2014-10-25 14:01:27 -0400

    Beautify 'featured' loop

--- partials/loop-featured.php
+++ partials/loop-featured.php
@@ -9,9 +9,15 @@
             <div class="col-sm-5 col-sm-push-7">
                 <div class="feature-slide feature-init"></div>
                 <div class="my-panel">
-                    <?php query_posts('category_name=featured&showposts=1'); ?>
-                    <?php while (have_posts()) : the_post(); ?>
-                    <?php the_post_thumbnail('portfolio-post-thumbnail', array('class' => "img-responsive")); ?>
+                    <?php
+
+                    query_posts( 'category_name=featured&showposts=1' );
+
+                    while ( have_posts() ) : the_post();
+                    the_post_thumbnail(
+                        "portfolio-post-thumbnail",
+                        array( "class" => "img-responsive" )
+                    ); ?>
                     <span><?php the_title(); ?></span>
                     <button type="button" class="btn btn-default" onclick="location.href='<?php echo get_permalink(); ?>';">More on the blog</button>
                     <?php endwhile; wp_reset_query(); ?>
