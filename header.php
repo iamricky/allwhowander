9b2063ed359eecefd019a0c0517354ae681c4722
heynicecode <rickymuldrew@gmail.com>
2014-10-25 11:10:53 -0400

    Create template part for featured loop

--- header.php
+++ header.php
@@ -12,30 +12,9 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<nav class="navbar navbar-default navbar-fixed-top hidden-xs" role="navigation">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-2 col-sm-push-10 feature-init feature-copy">
-						<p>Featured Event</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-5 col-sm-push-7">
-						<div class="feature-slide feature-init"></div>
-						<div class="my-panel">
-							<?php query_posts('category_name=featured&showposts=1'); ?>
-							<?php while (have_posts()) : the_post(); ?>
-							<?php the_post_thumbnail('portfolio-post-thumbnail', array('class' => "img-responsive")); ?>
-							<span><?php the_title(); ?></span>
-							<button type="button" class="btn btn-default" onclick="location.href='<?php echo get_permalink(); ?>';">More on the blog</button>
-							<?php endwhile; wp_reset_query(); ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
+		<?php get_template_part( "partials/loop", "featured"); ?>
 		<div class="body-padding-top hidden-xs"></div>
-				
+
 		<div class="container line-height"> <!-- container start -->
 			<div class="row my-nav">
 				<div class="logo visible-xs"><a href="www.allwhowanderevents.com"></a></div>
---
+++ partials/loop-featured.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-default navbar-fixed-top hidden-xs" role="navigation">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2 col-sm-push-10 feature-init feature-copy">
+                <p>Featured Event</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-5 col-sm-push-7">
+                <div class="feature-slide feature-init"></div>
+                <div class="my-panel">
+                    <?php query_posts('category_name=featured&showposts=1'); ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                    <?php the_post_thumbnail('portfolio-post-thumbnail', array('class' => "img-responsive")); ?>
+                    <span><?php the_title(); ?></span>
+                    <button type="button" class="btn btn-default" onclick="location.href='<?php echo get_permalink(); ?>';">More on the blog</button>
+                    <?php endwhile; wp_reset_query(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
