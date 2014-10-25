32b9d03c19d476e8c2383cc9753948e8b021d5e3
heynicecode <rickymuldrew@gmail.com>
2014-10-25 19:36:47 -0400

    Fixed

--- category.php
+++ category.php
@@ -2,12 +2,12 @@
 
 get_header();
 
-$category   = single_cat_title( "", false );
+$category   = strtolower( single_cat_title( "", false ) );
 $body_tags  = "col-sm-8 blog-pg";
 
 ?>
 
-    <!-- category title -->
+    <!-- title -->
     <div class="row headline">
         <h1><?php echo $category ?></h1>
         <img src="<?php bloginfo( "template_url" ); ?>/img/social-stroke.png" class="img-responsive social-stroke-align hidden-xs">
@@ -16,7 +16,7 @@ $body_tags  = "col-sm-8 blog-pg";
     <section class="<?php echo $body_tags; ?>">
         <?php if ( have_posts() ) : ?>
 
-            <!-- category posts -->
+            <!--  posts -->
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( "partials/post", $category ); ?>
             <?php endwhile; ?>
