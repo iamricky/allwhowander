41777918da39a29c7ec4c2ce1af8b7c99e7a6128
heynicecode <rickymuldrew@gmail.com>
2014-11-03 13:18:40 -0500

    Add missing loop

--- single.php
+++ single.php
@@ -15,13 +15,13 @@ if ( in_category( "blog" ) ) {
         <h1><?php echo $page_title; ?></h1>
         <img src="<?php bloginfo( "template_url" ); ?>/img/social-stroke.png" class="img-responsive social-stroke-align hidden-xs">
     </div>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <section class="row no-margin <?php echo $body_tags; ?>">
+            <?php get_template_part( "partials/single", $page_title ); ?>
+        </section>
 
-    <section class="row no-margin <?php echo $body_tags; ?>">
-        <?php get_template_part( "partials/single", $page_title ); ?>
-    </section>
-
-    <?php if ( in_category( "blog" ) ) : ?>
-        <?php get_template_part( "partials/layout", "sidebar" ); ?>
-    <?php endif; ?>
-
+        <?php if ( in_category( "blog" ) ) : ?>
+            <?php get_template_part( "partials/layout", "sidebar" ); ?>
+        <?php endif; ?>
+    <?php endwhile; endif; ?>
 <?php get_footer(); ?>
