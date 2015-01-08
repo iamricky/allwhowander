a48fb0beaf6fcf86c74b0a8c2b66a16cd73c42d1
heynicecode <rickymuldrew@gmail.com>
2015-01-08 01:44:26 -0500

    fix nav + jumbotron for mobile

--- header.php
+++ header.php
@@ -3,10 +3,10 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title><?php bloginfo('name'); ?></title>
+		<title><?php bloginfo( "name" ); ?></title>
 
-		<link href="<?php bloginfo("template_url"); ?>/style.css" rel="stylesheet">
-		<script type="text/javascript" src="<?php bloginfo("template_url"); ?>/js/respond.min.js"></script>
+		<link href="<?php bloginfo( "template_url" ); ?>/style.css" rel="stylesheet">
+		<script type="text/javascript" src="<?php bloginfo( "template_url" ); ?>/js/respond.min.js"></script>
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" type="image/x-icon" />
 		<?php wp_head(); ?>
 	</head>
@@ -17,6 +17,6 @@
 		<div class="container line-height"> <!-- container start -->
 			<div class="row my-nav">
 				<div class="logo visible-xs"><a href="www.allwhowanderevents.com"></a></div>
-				<?php wp_nav_menu(); ?>
+				<?php wp_nav_menu( array( "container_class" => "hidden-sm" ) ); ?>
 				<div class="logo hidden-xs"><a href="www.allwhowanderevents.com"></a></div>
 			</div>
