bdcbc400fee1be4cb49a20020cc9b2403bdb2394
heynicecode <rickymuldrew@gmail.com>
2015-01-08 22:37:06 -0500

    fix logo for mobile

--- header.php
+++ header.php
@@ -15,10 +15,14 @@
 		<div class="body-padding-top hidden-xs"></div>
 
 		<div class="container line-height"> <!-- container start -->
-			<div class="row my-nav">
-				<?php if ( is_home() ): ?>
-					<div class="logo visible-xs"><a href="www.allwhowanderevents.com"></a></div>
-				<?php endif; ?>
-				<?php wp_nav_menu( array( "container" => false, "menu_class" => "hidden-xs hidden-sm" ) ); ?>
-				<div class="logo hidden-xs"><a href="www.allwhowanderevents.com"></a></div>
+
+			<?php if ( is_home() ): ?>
+			<div class="row my-nav visible-xs">
+				<div class="logo"><a href="www.allwhowanderevents.com"></a></div>
+			</div>
+			<?php endif; ?>
+
+			<div class="row my-nav hidden-xs hidden-sm">
+				<?php wp_nav_menu( array( "container" => false ) ); ?>
+				<div class="logo"><a href="www.allwhowanderevents.com"></a></div>
 			</div>
